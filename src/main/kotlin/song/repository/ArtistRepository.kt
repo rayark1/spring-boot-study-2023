6df68641507739f61769642130231b4d899d62c8
jon <lgock2023@gmail.com>
2023-10-22 02:00:44 +0900

    setting week2

--- src/main/kotlin/song/repository/ArtistRepository.kt
+++ src/main/kotlin/song/repository/ArtistRepository.kt
@@ -1,6 +1,5 @@
 package com.wafflestudio.seminar.spring2023.song.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 interface ArtistRepository : JpaRepository<ArtistEntity, Long>
