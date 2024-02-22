b3f68885202db3b43a66f5c91bb06f1689856c33
rayark1 <max980203@snu.ac.kr>
2024-02-22 19:22:01 +0900

    feat: user signup, signin, authenticate 구현

--- src/main/kotlin/user/controller/UserController.kt
+++ src/main/kotlin/user/controller/UserController.kt
@@ -1,7 +1,8 @@
 package com.wafflestudio.seminar.spring2023.user.controller
 
-import com.wafflestudio.seminar.spring2023.user.service.UserService
+import com.wafflestudio.seminar.spring2023.user.service.*
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -17,21 +18,56 @@ class UserController(
     fun signup(
         @RequestBody request: SignUpRequest,
     ): ResponseEntity<Unit> {
-        TODO()
+        val user = userService.signUp(
+            username = request.username,
+            password = request.password,
+            image = request.image,
+        )
+        return ResponseEntity.ok(Unit)
     }
 
     @PostMapping("/api/v1/signin")
     fun signIn(
         @RequestBody request: SignInRequest,
     ): ResponseEntity<SignInResponse> {
-        TODO()
+        val user = userService.signIn(
+            username = request.username,
+            password = request.password,
+        )
+
+        return ResponseEntity.ok(SignInResponse(
+            accessToken = user.getAccessToken(),
+        ))
     }
 
     @GetMapping("/api/v1/users/me")
     fun me(
         @RequestHeader(name = "Authorization", required = false) authorizationHeader: String?,
     ): ResponseEntity<UserMeResponse> {
-        TODO()
+        val accessToken = extractTokenFromHeader(authorizationHeader)
+        val user = userService.authenticate(accessToken)
+        return ResponseEntity.ok(UserMeResponse(
+            username = user.username,
+            image = user.image,
+        ))
+    }
+
+    private fun extractTokenFromHeader(authorizationHeader: String?): String {
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            throw AuthenticateException()
+        }
+        return authorizationHeader.removePrefix("Bearer ")
+    }
+
+    @ExceptionHandler
+    fun handleException(e: UserException): ResponseEntity<Unit> {
+        val status = when (e) {
+            is SignUpBadUsernameException, is SignUpBadPasswordException -> 400
+            is SignUpUsernameConflictException -> 409
+            is SignInUserNotFoundException, is SignInInvalidPasswordException -> 404
+            is AuthenticateException -> 401
+        }
+        return ResponseEntity.status(status).build()
     }
 }
 
